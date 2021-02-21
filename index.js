59f618c940cb8f8cecc1b5df29fe1f3bedbbc520
sonikvvv <51079681+sonikvvv@users.noreply.github.com>
2021-02-21 15:32:16 +0200

    Created separate functions for fill and show.

--- index.js
+++ index.js
@@ -1,26 +1,50 @@
 let x;
 let y;
 
+let grid = [];
+
 const calc = document.getElementById("btnCalculate");
 calc.onclick = () => {
-    x = parseInt(document.getElementById("width").value);
-    y = parseInt(document.getElementById("height").value);
+    x = document.getElementById("width").value;
+    y = document.getElementById("height").value;
 
+    fillArray();
+};
+
+function fillArray() {
+    if (x <= 100 && y <= 100) {
+        for (let i = 0; i < y; i++) {
+            let row = [];
+            for (let j = 0; j < x; j++) {
+                const rand = Math.random() < 0.5 ? 0 : 1;
+                row.push(rand);
+            }
+            grid.push(row);
+        }
+        show(false);
+    }
+}
+
+function show(colors) {
     let table = document.createElement("table");
 
     for (let i = 0; i < y; i++) {
         let row = document.createElement("tr");
         for (let j = 0; j < x; j++) {
-            const rand = Math.random() < 0.5 ? 0 : 1;
             let cell = document.createElement("td");
-            cell.innerText = rand;
-            // rand == 0 ? cell.classList.add("red") : cell.classList.add("green");
+            let tmp = grid[i];
+            cell.innerText = tmp[j];
+            if (colors) {
+                rand == 0
+                    ? cell.classList.add("red")
+                    : cell.classList.add("green");
+            }
             row.appendChild(cell);
         }
         table.appendChild(row);
     }
 
     const section = document.getElementById("grid");
-    section.innerHTML = '';
+    section.innerHTML = "";
     section.appendChild(table);
-};
+}
